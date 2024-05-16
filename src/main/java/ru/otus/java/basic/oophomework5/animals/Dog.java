667482bf6cbfb2e7968ef93fc6f2f01f1322b31e
Andrey Melnik <andreum.melnik@yandex.ru>
2024-05-16 23:22:20 +0300

    Доомашнее задание 5

---
+++ src/main/java/ru/otus/java/basic/oophomework5/animals/Dog.java
@@ -0,0 +1,42 @@
+package ru.otus.java.basic.oophomework5.animals;
+
+public class Dog extends Animal {
+
+    public Dog(String name, int runningSpeed, int swimmingSpeed, int endurance) {
+        super(name, runningSpeed, swimmingSpeed, endurance);
+    }
+
+    @Override
+    public int run (int distance) {
+        if (endurance >= distance) {
+            time = distance / runningSpeed;
+            endurance = endurance - distance;
+            System.out.println("пробежал " + distance + "м за " + time + " секунд");
+        } else {
+            distance = endurance;
+            time = distance / runningSpeed;
+            endurance = 0;
+            System.out.println(name + " устал и смог пробежать " + distance + "м за " + time + " секунд");
+            time = -1;
+        }
+        return time;
+    }
+
+    @Override
+    public int swim (int distance) {
+        if (endurance >= (distance * 2)) {
+            time = distance / swimmingSpeed;
+            endurance = endurance - distance * 2;
+            System.out.println("проплыл " + distance + "м за " + time + " секунд");
+        } else {
+            distance = endurance / 2;
+            time = distance / runningSpeed;
+            endurance = 0;
+            System.out.println(name + " устал и смог проплыть " + distance + "м за " + time + " секунд");
+            time = -1;
+        }
+        return time;
+    }
+
+}
+
