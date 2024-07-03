d247ab424e0dc5bfad1f47d46286b3f7d7ce83de
Andrey Melnik <andreum.melnik@yandex.ru>
2024-07-03 13:26:31 +0300

    - Исправлено задание по замечаниям.

--- src/main/java/ru/otus/java/basic/homework14/Threads.java
+++ src/main/java/ru/otus/java/basic/homework14/Threads.java
@@ -1,6 +1,6 @@
 package ru.otus.java.basic.homework14;
 
-public class Threads extends Thread {
+public class Threads {
 
     public long timeThreads() {
 
@@ -8,19 +8,19 @@ public class Threads extends Thread {
         double[] array = new double[100_000_000];
 
         Thread t1 = new Thread(() -> {
-            for (int i = 0; i < array[24_999_999]; i++) {
+            for (int i = 0; i < array[25_000_000]; i++) {
                 array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
             }
         });
 
         Thread t2 = new Thread(() -> {
-            for (int i = 25_000_000; i < array[49_999_999]; i++) {
+            for (int i = 25_000_000; i < array[50_000_000]; i++) {
                 array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
             }
         });
 
         Thread t3 = new Thread(() -> {
-            for (int i = 50_000_000; i < array[74_999_999]; i++) {
+            for (int i = 50_000_000; i < array[75_000_000]; i++) {
                 array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
             }
         });
@@ -35,6 +35,15 @@ public class Threads extends Thread {
         t2.start();
         t3.start();
         t4.start();
+
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+            t4.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         return System.currentTimeMillis() - startTime;
 
